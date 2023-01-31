e78dc0701f0782821cb3d220585484cba89758bc
Oleksii Bolotin <samkiral73@gmail.com>
2023-01-31 01:25:04 +0100

    finish task

--- src/js/onInput.js
+++ src/js/onInput.js
@@ -1,10 +1,10 @@
 import ApiService from './fetchCountries';
 import clearFields from './clearFields';
 export default function onInput(e) {
-  if (!e.data) {
-    clearFields();
-    return;
-  }
+  // if (!e.data) {
+  //   clearFields();
+  //   return;
+  // }
   const api = new ApiService(e.target.value.trim());
   if (api.word) {
     api.fetchToApi();
